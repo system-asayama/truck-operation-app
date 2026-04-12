581eed248b36fc7ad68c6bdf427900aa53b37711
system.asayama <system.asayama@gmail.com>
2026-04-12 07:48:53 -0400

    fix: newArchEnabled=true、react-native-screensをv4.10.0にダウングレード

--- app.config.ts
+++ app.config.ts
@@ -46,7 +46,7 @@ const config: ExpoConfig = {
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
   userInterfaceStyle: "automatic",
-  newArchEnabled: false,
+  newArchEnabled: true,
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
